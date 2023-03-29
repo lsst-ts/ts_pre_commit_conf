475524231b72c022501a5e09e39e0d21daf7a60b
Wouter van Reeven <wvanreeven@lsst.org>
2023-03-29 18:07:56 +0200

    Rely on .ts_pre_commit_config.yaml for configuring the pre-commit hooks.

--- python/lsst/ts/pre_commit_conf/pre_commit_conf_generator.py
+++ python/lsst/ts/pre_commit_conf/pre_commit_conf_generator.py
@@ -25,6 +25,8 @@ import shutil
 import sys
 import types
 
+import yaml
+
 __all__ = [
     "DOT_GITIGNORE",
     "FLAKE8_CONFIG_FILE",
@@ -34,15 +36,23 @@ __all__ = [
     "MYPY_CONFIG_FILE",
     "MYPY_CONFIG_FILE_NAME",
     "PRE_COMMIT_CONFIG_FILE_NAME",
+    "TS_PRE_COMMIT_CONFIG_YAML",
     "copy_config_files",
+    "create_or_report_missing_config_file",
     "generate_pre_commit_conf",
     "generate_pre_commit_conf_file",
     "parse_args",
+    "update_args_from_config_file",
     "update_dot_gitignore",
+    "validate_config_file_contents",
 ]
 
 # The root of the current file to be used by the Path definitions below.
 ROOT = pathlib.Path(__file__)
+
+# The YAML file holding the configuration for the "generate_pre_commit_conf"
+# command.
+TS_PRE_COMMIT_CONFIG_YAML = ".ts_pre_commit_config.yaml"
 
 # Directories with data used by this script.
 CONFIG_FILES_DIR = ROOT.resolve().parents[0] / "config_files"
@@ -55,6 +65,7 @@ MYPY_CONFIG_FILE_NAME = ".mypy.ini"
 PRE_COMMIT_CONFIG_FILE_NAME = ".pre-commit-config.yaml"
 
 # Config file paths for the pre-commit hooks.
+TS_PRE_COMMIT_CONFIG_YAML_FILE = CONFIG_FILES_DIR / TS_PRE_COMMIT_CONFIG_YAML
 FLAKE8_CONFIG_FILE = CONFIG_FILES_DIR / FLAKE8_CONFIG_FILE_NAME
 ISORT_CONFIG_FILE = CONFIG_FILES_DIR / ISORT_CONFIG_FILE_NAME
 MYPY_CONFIG_FILE = CONFIG_FILES_DIR / MYPY_CONFIG_FILE_NAME
@@ -65,6 +76,17 @@ MYPY_PRE_COMMIT_HOOK_TEMPLATE = TEMPLATES_DIR / "mypy-pre-commit-hook-template.y
 PRE_COMMIT_CONFIG_TEMPLATE = TEMPLATES_DIR / "pre-commit-config-template.yaml"
 
 DOT_GITIGNORE = ".gitignore"
+
+# Mandatory pre-commit hooks for TSSW.
+MANDATORY_PRE_COMMIT_HOOKS = frozenset(
+    ("check-yaml", "check-xml", "black", "flake8", "isort")
+)
+
+# Optional pre-commit hooks for TSSW and the arg used for them.
+OPTIONAL_PRE_COMMIT_HOOKS = {"mypy": "no_mypy"}
+
+# All pre-commit hooks for TSSW.
+ALL_PRE_COMMIT_HOOKS = MANDATORY_PRE_COMMIT_HOOKS.union(OPTIONAL_PRE_COMMIT_HOOKS)
 
 
 def parse_args(command_line_args: list[str]) -> types.SimpleNamespace:
@@ -92,7 +114,16 @@ def parse_args(command_line_args: list[str]) -> types.SimpleNamespace:
         action="store_true",
         default=False,
         help="Generate a pre-commit configuration file without mypy "
-        "(default: False, meaning mypy gets included).",
+        "(default: False, meaning mypy gets included). This options requires "
+        "--create to be specified as well.",
+    )
+
+    parser.add_argument(
+        "--create",
+        action="store_true",
+        default=False,
+        help="Create a .ts_pre_commit_conf.yaml configuration file "
+        "(default: False, meaning no configuration file is created).",
     )
 
     parser.add_argument(
@@ -107,6 +138,9 @@ def parse_args(command_line_args: list[str]) -> types.SimpleNamespace:
     sn = types.SimpleNamespace()
     for var in vars(args):
         setattr(sn, var, getattr(args, var))
+    if sn.no_mypy is True and sn.create is False:
+        raise ValueError("Specifying --no-mypy requires --create as well.")
+
     return sn
 
 
@@ -130,6 +164,188 @@ def _get_dest(args: types.SimpleNamespace) -> pathlib.Path:
     return dest
 
 
+def create_or_report_missing_config_file(args: types.SimpleNamespace) -> None:
+    """Verify that the .ts_pre_commit_config.yaml config file exists and either
+    create one or report that it is missing, depending on the provided args.
+
+    Parameters
+    ----------
+    args : `types.SimpleNamespace`
+        The args that determine the destination path and whether a config file
+        should be created if it is missing.
+
+    Raises
+    ------
+    FileNotFoundError
+        In case the config file doesn't exist and will not be created. The
+        error message contains the instructions for how to create the missing
+        config file.
+    """
+    dest = _get_dest(args=args)
+    config_path = dest / TS_PRE_COMMIT_CONFIG_YAML
+    if not config_path.exists():
+        print(f"No config file {TS_PRE_COMMIT_CONFIG_YAML} found.", end=" ")
+        if args.create is True:
+            _create_config_file(args)
+        else:
+            _print_instructions_and_exit(args)
+
+
+def _create_config_file(args: types.SimpleNamespace) -> None:
+    """Create a .ts_pre_commit_config.yaml config file with contents based on
+    the provided arguments.
+
+    Parameters
+    ----------
+    args : `types.SimpleNamespace`
+        The args that determine the destination path and contents of the config
+        file.
+    """
+    dest = _get_dest(args=args)
+    print("Creating one now.")
+    shutil.copy(TS_PRE_COMMIT_CONFIG_YAML_FILE, dest)
+    if args.no_mypy is True:
+        with open(dest / TS_PRE_COMMIT_CONFIG_YAML, "r") as f:
+            lines = f.read()
+
+        lines = lines.replace("mypy: true", "mypy: false")
+
+        with open(dest / TS_PRE_COMMIT_CONFIG_YAML, "w") as f:
+            f.write(lines)
+
+
+def _print_instructions_and_exit(args: types.SimpleNamespace) -> None:
+    """Print instructions for how to create a .ts_pre_commit_config.yaml config
+    file and exit.
+
+    Parameters
+    ----------
+    args : `types.SimpleNamespace`
+        The args that determine the destination path and contents of the config
+        file.
+
+    Raises
+    ------
+    FileNotFoundError
+        The error message contains the instructions for how to create the
+        missing config file.
+    """
+    message = "Create one by copying and pasting the following lines:\n"
+    message = message + "\n"
+    message = message + "check-yaml: true\n"
+    message = message + "check-xml: true\n"
+    message = message + "black: true\n"
+    message = message + "flake8: true\n"
+    message = message + "isort: true\n"
+    message = message + f"mypy: {'true' if args.no_mypy is False else 'false'}"
+    message = message + "\n"
+    raise FileNotFoundError(message)
+
+
+def validate_config_file_contents(args: types.SimpleNamespace) -> None:
+    """Validate the contents of the .ts_pre_commit_config.yaml config file. The
+    following pre-commit hooks need to be present in the config file:
+
+        check-yaml
+        check-xml
+        black
+        flake8
+        isort
+        mypy
+
+    All pre-commit hooks are mandatory, so all need to be set to "yes", with
+    the exception of mypy, which may be set to "no". No additional pre-commit
+    hooks may be defined.
+
+    Parameters
+    ----------
+    args : `types.SimpleNamespace`
+        The args that determine the path to the config file.
+
+    Raises
+    ------
+    ValueError
+        In case of incorrect, missing or not allowed pre-commit hook config
+        options.
+
+    Notes
+    -----
+    The script will fail with a comprehensive error message if any of the
+    pre-commit hooks used by TSSW are missing, if any of the mandatory
+    pre-commit hooks are set to "no" or if any additional pre-commit hooks
+    (meaning any non-empty line that is not a comment) is present.
+    """
+    dest = _get_dest(args=args)
+    with open(dest / TS_PRE_COMMIT_CONFIG_YAML) as f:
+        config = yaml.safe_load(f)
+
+    missing_hooks = []
+    incorrect_config_options = []
+    additional_hooks = []
+    for hook in ALL_PRE_COMMIT_HOOKS:
+        missing = True
+        for option in config:
+            if option == hook:
+                missing = False
+                break
+        if missing:
+            missing_hooks.append(hook)
+    for hook in MANDATORY_PRE_COMMIT_HOOKS:
+        incorrect_config_option = True
+        for option in config:
+            if option == hook and config[option] is True:
+                incorrect_config_option = False
+                break
+        if incorrect_config_option:
+            incorrect_config_options.append(hook)
+    for option in config:
+        additional_hook = True
+        for hook in ALL_PRE_COMMIT_HOOKS:
+            if option == hook:
+                additional_hook = False
+                continue
+        if additional_hook:
+            additional_hooks.append(option)
+
+    exit_messages = []
+    if missing_hooks:
+        exit_messages.append(
+            f"The following pre-commit hooks are missing: {sorted(missing_hooks)}"
+        )
+    if incorrect_config_options:
+        exit_messages.append(
+            "The following mandatory pre-commit hooks are "
+            + f"not set to 'true': {sorted(incorrect_config_options)}"
+        )
+    if additional_hooks:
+        exit_messages.append(
+            f"Please remove the following additional lines: {additional_hooks}"
+        )
+
+    if missing_hooks or incorrect_config_options or additional_hooks:
+        raise ValueError("\n".join(exit_messages))
+
+
+def update_args_from_config_file(args: types.SimpleNamespace) -> None:
+    """Read the .ts_pre_commit_config.yaml config file and update the args
+    accordingly.
+
+    Parameters
+    ----------
+    args : `types.SimpleNamespace`
+        The args that determine the destination path and the contents of the
+        config file.
+    """
+    dest = _get_dest(args=args)
+    with open(dest / TS_PRE_COMMIT_CONFIG_YAML) as f:
+        config = yaml.safe_load(f)
+
+    for hook in OPTIONAL_PRE_COMMIT_HOOKS:
+        option = config[hook]
+        arg = OPTIONAL_PRE_COMMIT_HOOKS[hook]
+        setattr(args, arg, not option)
+
+
 def generate_pre_commit_conf_file(args: types.SimpleNamespace) -> None:
     """Generate the '.pre-commit-conif.yaml' file. Both the contents and the
     destination path are determined from the provided args.
@@ -142,7 +358,7 @@ def generate_pre_commit_conf_file(args: types.SimpleNamespace) -> None:
     dest = _get_dest(args=args)
     with open(PRE_COMMIT_CONFIG_TEMPLATE) as f:
         pre_commit_config = f.read()
-    if "no_mypy" not in vars(args) or args.no_mypy is False:
+    if args.no_mypy is False:
         with open(MYPY_PRE_COMMIT_HOOK_TEMPLATE) as f:
             pre_commit_config = pre_commit_config + f.read()
     with open(pathlib.Path(dest) / PRE_COMMIT_CONFIG_FILE_NAME, "w") as f:
@@ -202,7 +418,13 @@ def generate_pre_commit_conf() -> None:
     .gitignore file.
     """
     command_line_args = sys.argv[1:]
-    args = parse_args(command_line_args=command_line_args)
+    try:
+        args = parse_args(command_line_args=command_line_args)
+        create_or_report_missing_config_file(args)
+        validate_config_file_contents(args)
+    except (ValueError, FileNotFoundError) as e:
+        sys.exit(str(e))
+    update_args_from_config_file(args)
     generate_pre_commit_conf_file(args)
     copy_config_files(args)
     update_dot_gitignore(args)
